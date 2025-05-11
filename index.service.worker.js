cd912ad1198aef95b06b35190951030adabb8520
Alexander Kunkel <kunkel.alexander@protonmail.com>
2025-05-11 10:21:03 +0200

    Make glow red

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746899862|1406066497';
+const CACHE_VERSION = '1746951640|5471805067';
 /** @type {string} */
 const CACHE_PREFIX = 'Neon Sand-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- scripts/gpu_renderer.gd
+++ scripts/gpu_renderer.gd
@@ -197,7 +197,7 @@ func create_color_palette_texture():
 		var interpolated = c1.lerp(c2, frac)
 		palette_image.set_pixel(i, 0, interpolated)
 	# Bloom will mix with this
-	palette_image.set_pixel(0, 0, Color(0, 0, 0, 0))
+	palette_image.set_pixel(0, 0, Color(1, 0, 0, 0))
 	color_palette_texture = ImageTexture.create_from_image(palette_image)
 
 func create_random_color_palette_texture():
