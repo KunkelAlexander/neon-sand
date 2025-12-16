79ba27a234d6de4e2abfef1f783181af259df820
Alexander Kunkel <kunkel.alexander@protonmail.com>
2025-12-16 17:12:04 +0100

    Clean up some more

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765901109|2013734794';
+const CACHE_VERSION = '1765901434|2338234123';
 /** @type {string} */
 const CACHE_PREFIX = 'Neon Sand-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
