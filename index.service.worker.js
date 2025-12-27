baa6036a0709aa3241fb51386e261ca068c26687
Alexander Kunkel <kunkel.alexander@protonmail.com>
2025-12-27 11:29:49 +0100

    Decrease simulation resolution for better performance on high-resolution devices

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765901434|2338234123';
+const CACHE_VERSION = '1766831334|35316251';
 /** @type {string} */
 const CACHE_PREFIX = 'Neon Sand-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- scripts/global.gd
+++ scripts/global.gd
@@ -2,9 +2,6 @@ extends Node
 
 # Sand type definitions
 const SAND_EMPTY = 0
-const SAND_TYPE1 = 1  # e.g. white sand
-const SAND_TYPE2 = 2  # e.g. red sand
-const SAND_TYPE3 = 3  # e.g. green sand
 
 
-const SIM_SCALE = 2
+const SIM_SCALE = 4
